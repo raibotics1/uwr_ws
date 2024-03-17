ff97971ed037a34819218698930d0d5f0a3b324b
RAIBOTICS <85551218+raibotics1@users.noreply.github.com>
2024-03-18 02:51:13 +0300

    Add files via upload
    add uwr_robot_description package

---
+++ uwr_robot_description/launch/robot_description.launch.py
@@ -0,0 +1,39 @@
+
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    urdf_file_name = 'uwr_robot.xacro'
+    urdf = os.path.join(
+        get_package_share_directory('uwr_robot_description'),
+        'urdf',
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc}],
+        arguments=[urdf]
+    )
+  
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+    )
+
+    return launch.LaunchDescription([
+        robot_state_publisher_node,
+        rviz_node
+    ])
